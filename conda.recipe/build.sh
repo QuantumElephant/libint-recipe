ad8003e3bb5fa4809cbfa5ce9ad2217df6189c79
Matthew Chan <chanmhy@mcmaster.ca>
2018-09-20 01:39:28 -0400

    libint 2.0.3 build

---
+++ conda.recipe/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+env
+echo $PREFIX
+
+export CC="ccache $CC"
+export CXX="ccache $CXX"
+
+# configure
+./configure --with-cxx-optflags='-O1' --prefix=${PREFIX} --enable-shared=yes --enable-static=no
+
+# build
+make -j${CPU_COUNT}
+
+# install
+make install
+
+# check
+LD_LIBRARY_PATH=${PREFIX}/lib make check
