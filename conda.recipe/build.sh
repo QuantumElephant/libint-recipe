2fa4272569165d9ec0e77e812c562d7957335421
Matthew Chan <chanmhy@mcmaster.ca>
2018-11-02 14:41:56 -0400

    check before installing, to work around GH#114

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -6,14 +6,14 @@ export CC="ccache $CC"
 export CXX="ccache $CXX"
 
 # configure
-./configure --with-cxx-optflags='-O1' --prefix=${PREFIX} --enable-shared=yes --enable-static=yes
+./configure --with-cxx-optflags='-O1' --prefix=${PREFIX} --enable-shared=yes --enable-static=no
 
 # build
 make -j${CPU_COUNT}
+make check
 
 # install
 make install
 
 # check
 #LD_LIBRARY_PATH=${PREFIX}/lib make check
-make check
